e871c3f6dbb6fd0fa91bf15733455dd7a5a5aa25
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-05-18 16:04:41 +0500

    Create exceptions.ts

---
+++ src/api/consts/exceptions.ts
@@ -0,0 +1,11 @@
+export default {
+    API_TEST: [-9999, 'API exception error'],
+    API_REQUEST_PARAMS_INVALID: [-2000, 'The request parameter is illegal'],
+    API_REQUEST_FAILED: [-2001, 'Request failed'],
+    API_TOKEN_EXPIRES: [-2002, 'Token has expired'],
+    API_FILE_URL_INVALID: [-2003, 'The remote file URL is illegal'],
+    API_FILE_EXECEEDS_SIZE: [-2004, 'Remote file exceeds size'],
+    API_CHAT_STREAM_PUSHING: [-2005, 'There is already a conversation flow being output'],
+    API_CONTENT_FILTERED: [-2006, 'Content has been blocked due to compliance issues'],
+    API_IMAGE_GENERATION_FAILED: [-2007, 'Image generation failed']
+}
